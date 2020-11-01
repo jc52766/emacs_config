ff0e2687de800ea3f9e4aa5e6933000a058c7f4c
Jason Corderoy <analytics.jc@gmail.com>
2020-11-01 17:11:45 +1100

    elpy and theme changes added.

--- init.el
+++ init.el
@@ -9,6 +9,8 @@
 
 (package-initialize)
 
+(load-theme 'zenburn t)
+
 ;; Download Evil
 (unless (package-installed-p 'evil)
   (package-install 'evil))
@@ -16,6 +18,12 @@
 ;; Enable Evil
 (require 'evil)
 (evil-mode 1)
+
+;; elpy (for python projects)
+(use-package elpy
+  :ensure t
+  :init
+  (elpy-enable))
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 
@@ -40,10 +48,10 @@ names an existing file."
     ("f2c35f8562f6a1e5b3f4c543d5ff8f24100fae1da29aeb1864bbc17758f52b70" "b89a4f5916c29a235d0600ad5a0849b1c50fab16c2c518e1d98f0412367e7f97" default)))
  '(package-selected-packages
    (quote
-    (zenburn-theme slime live-py-mode j-mode helm-themes evil cyberpunk-theme cider autothemer))))
+    (elpy zenburn-theme slime live-py-mode j-mode helm-themes evil cyberpunk-theme cider autothemer))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Math TeX Gyre" :foundry "Deja" :slant normal :weight normal :height 120 :width normal)))))
+ '(default ((t (:family "DejaVu Math TeX Gyre" :foundry "Deja" :slant normal :weight normal :height 130 :width normal)))))
